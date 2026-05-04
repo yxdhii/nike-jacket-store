ff7df9add71bd307973a67842a2199d95eaeaea4
yxdhii <yadhirasaavedra069@gmail.com>
2026-05-03 20:44:12 -0500

    refactor: improve code in NikeSwoosh.jsx

--- src/components/NikeSwoosh.jsx
+++ src/components/NikeSwoosh.jsx
@@ -1,3 +1,16 @@
-export default function NikeSwoosh() {
-  return <div>Swoosh</div>
+export default function NikeSwoosh({ className = "", opacity = 1 }) {
+  return (
+    <svg
+      viewBox="0 0 200 130"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      style={{ opacity }}
+    >
+      <path
+        d="M18 92 Q62 18 192 8 Q118 52 8 102 Z"
+        fill="white"
+      />
+    </svg>
+  );
 }
